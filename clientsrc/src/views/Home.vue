19a075bc3dd7a09d2b8b685341ca59b96322e033
Ben W <bwiedeback@gmail.com>
2020-09-24 15:49:15 -0600

    more styling and a new picture

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home d-flex flex-column justify-content-center">
-    <h1 class="text-light text-shadow">Welcome to your Home Page!</h1>
+  <div class="home d-flex flex-column justify-content-center align-items-center">
+    <h1 class="text-light text-shadow shadow see-through p-2"><i class="fas fa-torii-gate"></i> Welcome to your Home Page! <i class="fas fa-torii-gate"></i></h1>
   </div>
 </template>
 
@@ -20,12 +20,16 @@ export default {
 
 <style scoped>
   .home{
-    background-image: url("~@/assets/imgs/ocean.jpg");
+    background-image: url("~@/assets/imgs/gate.jpg");
     background-position: center;
     background-size: cover;
     height: 100vh;
   }
   .text-shadow{
-    text-shadow: 1px 1px #030303
+    text-shadow: 1.5px 1.5px 4px #030303
   }
+.see-through{
+    background-color: hsla(218, 19%, 89%, 0.75);
+  }
+
 </style>
